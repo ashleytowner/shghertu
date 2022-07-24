6ce34d0e2ea0750d1fed3988357d3654ca1aa863
Ashley Towner <13824257+ashleytowner@users.noreply.github.com>
2022-07-24 15:27:07 +0930

    added noscript fallbacks

--- public/entoggle.js
+++
@@ -1,9 +0,0 @@
-function toggle() {
-  const element = document.getElementsByTagName('html')[0];
-  const showEn = element.getAttribute('data-en')
-  if (showEn === 'true') {
-    element.setAttribute('data-en', 'false');
-  } else {
-    element.setAttribute('data-en', 'true');
-  }
-}
