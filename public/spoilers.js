944cbfb2e029229b6232941344a301043950f11f
Ashley Towner <git@ashtowner.com>
2023-09-20 01:08:16 +0930

    Added separate page for english translations

--- public/spoilers.js
+++
@@ -1,9 +0,0 @@
-const spoilerLang = new URLSearchParams(document.location.search).get('spoiler');
-if (spoilerLang) {
-  document.getElementsByTagName('html')[0].setAttribute('data-spoiler', spoilerLang);
-}
-
-const spoilers = document.getElementsByClassName('spoiler');
-for (let i = 0; i < spoilers.length; i++) {
-  spoilers[i].setAttribute('tabindex', '0');
-}
